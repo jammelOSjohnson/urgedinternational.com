8d1f0dca2eb4025c59a4f4694dce27119741adb2
JammelOSJohnson <jammeljohnson@gmail.com>
2021-07-07 22:31:57 -0500

    created Header and footer component

---
+++ packages/web/src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const Footer: React.FC = function Footer() {
+    return (
+        <div>
+            This is the footer
+        </div>
+    )
+}
---
+++ packages/web/src/Components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const Header: React.FC = function Header() {
+    return (
+        <div>
+            This is the header
+        </div>
+    )
+}
+
+ 
