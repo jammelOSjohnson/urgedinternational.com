a5d02dd6f562f2316e805a8f70a2c37b377e6915
JammelOSJohnson <jammeljohnson@gmail.com>
2021-07-07 22:30:30 -0500

    created about screen

---
+++ packages/web/src/Screens/AboutScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const AboutScreen: React.FC = function AboutScreen() {
+    return (
+        <div>
+            This is the about screen
+        </div>
+    )
+}
