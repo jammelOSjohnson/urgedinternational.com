08af28740410d2b7859ade1cb3011001c3e36534
JammelOSJohnson <jammeljohnson@gmail.com>
2021-07-07 22:34:15 -0500

    created main container component

---
+++ packages/web/src/Components/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Header } from '../Components/Header';
+import { Footer } from '../Components/Footer';
+//import { HomeScreen } from '../Screens/HomeScreen';
+import { AboutScreen } from '../Screens/AboutScreen';
+
+//Import provider
+import AppDataProvider from '../Context/AppDataContext';
+
+const App: React.FC = function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Header/>
+            <Route path="/" exact component={AboutScreen} />
+          <Footer/>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(){
+  return (
+    <AppDataProvider>
+      <App />
+    </AppDataProvider>
+  );
+};
