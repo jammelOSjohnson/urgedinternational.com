e8ffcb30a92a886c94e692ca64d9a32e5e9ca585
JammelOSJohnson <jammeljohnson@gmail.com>
2021-07-08 21:13:44 -0500

    installed mateiral ui and created components for about us page sections

--- packages/web/src/Components/App.tsx
+++ packages/web/src/Components/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Header } from '../Components/Header';
 import { Footer } from '../Components/Footer';
 //import { HomeScreen } from '../Screens/HomeScreen';
-import { AboutScreen } from '../Screens/AboutScreen';
+import { AboutScreen } from '../Screens/About/AboutScreen';
 
 //Import provider
 import AppDataProvider from '../Context/AppDataContext';
---
+++ packages/web/src/Screens/About/Comp/Section5.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const Section5: React.FC = function Section5() {
+    return (
+        <div>
+            Section5
+        </div>
+    )
+}
