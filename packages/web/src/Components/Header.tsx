4ef2c2806104b015811ef8e2163bf302a83986d0
Dwayne Redwood <dwayneredwood@live.com>
2021-07-11 03:28:52 -0500

    Added section 3 and some styling

--- packages/web/src/Components/Header.tsx
+++ packages/web/src/Components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Typography, AppBar, Button, Toolbar, makeStyles, Theme, createStyles, IconButton, Tabs, Tab } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
+import { CallMissedSharp } from '@material-ui/icons';
 
 interface Props {
     
@@ -9,28 +10,44 @@ interface Props {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-        fontFamily: "PT Sans",
+        fontFamily: "Open Sans",
         flexGrow: 1,
     },
     appbar: {
-        height: 125,
         backgroundColor: "#FFFFFF",
+        elevation: 0,
     },
     iconButton: {
     },
     logo: {
         height: 50.49015808105469,
         width: 147,        
-        borderRadius: 0,
     },
-    tabs: {
+    Typo1: {
+        fontWeight: 600,
+        fontSize: 16,
+        fontStyle: "normal",
+        lineHeight: "132.69%",
+        color: "#000000",
+        marginRight: theme.spacing(2),
     },
-    button: {
-        backgroundColor: "#F7B614",
-        height: 41,
-        width: 130,
-        borderRadius: 36,
-    },
+    // tabs: {
+    //     fontStyle: "normal",
+    //     fontWeight: "bold",
+    //     fontSize: 16,
+    //     lineHeight: 21,
+    //     color: "#444444",
+
+    // },
+    // button: {
+    //     backgroundColor: "#F7B614",
+    //     marginRight:theme.spacing(2),
+    //     height: 41,
+    //     width: 130,
+    //     borderRadius: 36,
+    //     color: "#FFFFFF",
+    // },
+    toolbar: theme.mixins.toolbar
   }),
 );
 
@@ -41,18 +58,25 @@ export const Header: React.FC = function Header() {
             <AppBar className={classes.appbar}>
                 <Toolbar>
                     <IconButton className={classes.iconButton}>
-                        <img className={classes.logo} src="Images/logo.svg" alt="Urged Logo"></img>
+                        <img className={classes.logo} src="Images/urged logo.svg" alt="Urged Logo"></img>
                     </IconButton>
-                    <Tabs textColor='secondary'>
+                    <Typography className={classes.Typo1}>
+                        876-773-5015 
+                    </Typography>
+                    <Typography className={classes.Typo1}>
+                        Monday - Saturday 9:00 am - 6:00pm
+                    </Typography>
+                    {/* <Tabs className={classes.tabs}>
                         <Tab label='Services'/>
                         <Tab label='Industries'/>
                         <Tab label='How it works'/>
                         <Tab label='Why'/>
-                    </Tabs>
-                    <Button className={classes.button}>Place Order</Button>
-                    <Button>Login</Button>
+                    </Tabs> */}
+                    {/* <Button className={classes.button}>Place Order</Button>
+                    <Button className={classes.button}>Login</Button> */}
                 </Toolbar>
             </AppBar>
+            <div className={classes.toolbar}></div>
         </>
     )
 }
