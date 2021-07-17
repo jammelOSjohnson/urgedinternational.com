4acd50c066ab435ae230a41f19ca499b36f10fb1
Dwayne Redwood <dwayneredwood@live.com>
2021-07-16 19:46:40 -0500

    Updated section 2 styling and added cards

--- packages/web/src/Screens/About/Comp/Section2.tsx
+++ packages/web/src/Screens/About/Comp/Section2.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography, makeStyles, Theme, createStyles } from '@material-ui/core'
+import { Box, Container, Grid, Typography, makeStyles, Theme, createStyles, Card, CardContent } from '@material-ui/core'
 import React from 'react'
 
 interface Props {
@@ -8,7 +8,6 @@ interface Props {
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
         root: {
-            // justifyContent: "center",
             height: "633px",
         },
         s2Background: {
@@ -19,6 +18,7 @@ const useStyles = makeStyles((theme: Theme) =>
             color: "#F7B614",
             fontSize: "40px",
             fontWeight: 700,
+            marginLeft: "auto"
         },
         s2Span: {
             color: "#000000",
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme: Theme) =>
             fontSize: "16px",
             lineHeight: "150%",
             color: "#3F3F50",
-            marginLeft: 155,
+            marginLeft: "auto",
         },
         Typo3: {
             fontFamily: "Open Sans",
@@ -50,17 +50,9 @@ const useStyles = makeStyles((theme: Theme) =>
             fontWeight: 600,
             fontSize: "25px",
             lineHeight: "150%",
-            marginRight: 155,
+            marginRight: "auto",
             color: "#1D2635"
-        },
-        // Button: {
-        //         backgroundColor: "#FAFAFA",
-        //         border: "1.21951px solid #F7B614",
-        //         height: 41,
-        //         width: 171,
-        //         borderRadius: 36,
-        //         color: "#F7B614",
-        //     },
+        }
     }),
 );
 
@@ -68,27 +60,53 @@ export const Section2: React.FC = function Section2() {
     const classes = useStyles();
     return (
      <>
-        <Grid container spacing={2} className={classes.root} justifyContent="center" alignItems="center">
-            <Grid item xs={12}>
+        <Grid container spacing={2} className={classes.root} alignItems="center">
+            <Grid item md={1}></Grid>
+            <Grid item xs={12} md={11}>
                 <Typography variant="h1" className={classes.s2Heading}>We Deliver,<span className={classes.s2Span}>You Enjoy.</span></Typography>
             </Grid>
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
                 <img className={classes.yellowBar1}></img>
+            </Grid> */}
+            <Grid item md={1}></Grid>
+            <Grid item xs={12} md={5}>
+                <Typography className={classes.Typo2}>Our customers come first. We provide quality delivery and errand services to ease their everyday lives.</Typography>
             </Grid>
-            <Grid item xs={12} md={6}>
-                <Typography paragraph={true} className={classes.Typo2}>Our customers come first. We provide quality delivery and errand services to ease their everyday lives.</Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <img className={classes.yellowBar2}></img>
+            <Grid item xs={12} md={5}>
+                {/* <img className={classes.yellowBar2}></img> */}
                 <Typography variant="h2" className={classes.Typo3}>Ease your everyday life, by allowing us to take care of all your errands and delivery services for you.</Typography>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item md={1}></Grid>
+
+            <Grid item xs={12} container spacing={2}>
                 <Grid item xs={6} md={3}>
-                    <Typography>We are reliable, so you can always count on us.</Typography>
-                    <Typography>Where ever you are, we got you covered. Speed is our priority</Typography>
-                    <Typography>We will take care of all your delivery and errand needs for you,</Typography>
-                    <Typography>We provide quality services, and will never let you down.</Typography>
-                </Grid>            
+                    <Card>
+                        <CardContent>
+                            <Typography>We are reliable, so you can always count on us.</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>  
+                <Grid item xs={6} md={3}>
+                    <Card>
+                        <CardContent>
+                            <Typography>Where ever you are, we got you covered. Speed is our priority</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>      
+                <Grid item xs={6} md={3}>              
+                    <Card>
+                        <CardContent>
+                            <Typography>We will take care of all your delivery and errand needs for you,</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>      
+                <Grid item xs={6} md={3}>              
+                    <Card>
+                        <CardContent>
+                            <Typography>We provide quality services, and will never let you down.</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>                              
             </Grid>
         </Grid>
      </>
