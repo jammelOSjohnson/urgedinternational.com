b6027e6dee55d9a5a8f68489c1c5169e6f636d68
JammelOSJohnson <jammeljohnson@gmail.com>
2021-09-19 18:54:04 -0500

    favourites ui completed

--- packages/web/src/Screens/Dashboard/Comp/Favourites.tsx
+++ packages/web/src/Screens/Dashboard/Comp/Favourites.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, makeStyles, createStyles, Typography, Theme, TextField, Button, Input, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl } from '@material-ui/core';
+import { Container, Grid, makeStyles, createStyles, Typography, Theme, TextField, Button, Input, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl, Card, CardMedia, CardContent } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import React from 'react';
@@ -18,7 +18,39 @@ interface State {
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
-        
+        favorites: {
+            fontWeight: "bold"
+        },
+        root: {
+            padding: "0% 0px 5% 0px"
+        },
+        card: {
+            background: "#FFFFFF",
+            border: "1.14582px solid #F3F3F3",
+            boxSizing: "border-box",
+            boxShadow: "0px 4.58327px 17.1873px rgba(0, 0, 0, 0.11)",
+            borderRadius: "34.3745px",
+            paddingLeft: 0,
+            paddingRight: 0,
+            paddingTop: "10px",
+            textAlign: "center"
+        },
+        cardContent: {
+            flexGrow: 1,
+            textAlign: "center",
+            paddingBottom: "0px",
+            paddingTop: "0px",
+        },
+        cardImage: {
+            textAlign: "center",
+            position: "relative"
+        },
+        cardTitle: {
+            fontSize: "20px",
+            fontWeight: 700,
+            color: "#1D2635",
+            fontFamily: "PT Sans",
+        },
     }),
 );
 
@@ -36,7 +68,97 @@ export const Favourites: React.FC = function Favourites() {
       
     return (
         <>
-            Favourites
+            <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                    <Grid container direction="row" spacing={3} className={classes.root} alignItems="center">
+                        <Grid item xs={12} container spacing={1} style={{overflowY: "scroll", height: "378.58px"}}>
+                            <Grid item xs={8} >
+                                <Typography variant="subtitle1" className={classes.favorites}>
+                                    Favourites
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={4} >
+                                <Typography variant="subtitle1" className={classes.favorites}>
+                                    View More
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleBigDeal1.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Big Deal
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleBigDeal2.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Big Deal
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleSalad.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Salad
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleBigDeal2.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Big Deal
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleBigDeal2.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Big Deal
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <Card className={classes.card}>
+                                    <CardMedia className={classes.cardImage}>
+                                        <img src="Images/ExampleBigDeal2.png"></img>
+                                    </CardMedia>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom className={classes.cardTitle}>
+                                            Big Deal
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+            
         </>
     )
 }
