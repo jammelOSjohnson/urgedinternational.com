613c3dce48613263d6a9f518d727daa2d724fbc6
JammelOSJohnson <jammeljohnson@gmail.com>
2021-10-03 22:01:01 -0500

    Slider UI Search Form UI

--- packages/web/src/Screens/Dashboard/Comp/AdvertisementSlider2.tsx
+++ packages/web/src/Screens/Dashboard/Comp/AdvertisementSlider2.tsx
@@ -1,4 +1,4 @@
-import { makeStyles, createStyles, Typography, Theme, Button, IconButton, Slider, useMediaQuery } from '@material-ui/core';
+import { makeStyles, createStyles, Typography, Theme, Button, IconButton, Slider, useMediaQuery, FormControl, OutlinedInput, InputAdornment, InputLabel, Select, MenuItem, TextField } from '@material-ui/core';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import clsx from 'clsx';
@@ -6,15 +6,16 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../CSS/FoodSlider.css";
 import { Link } from "react-router-dom";
+import { useAppData } from '../../../Context/AppDataContext';
+import { LocationOnRounded } from "@material-ui/icons/";
 
 interface Props {
     
 }
 
 interface State {
-    email: string;
-    password: string;
-    showPassword: boolean;
+    address: string;
+    searchquery: string;
 }
 
 const useStyles = makeStyles((theme: Theme) => 
@@ -47,7 +48,7 @@ const useStyles = makeStyles((theme: Theme) =>
             fontSize: 'clamp(16px, 3vw, 51.31px)',
             fontWeight: 700,
             lineHeight: '120%',
-            width: "50%",
+            width: "36%",
             textAlign: "left",
             //padding: '0% 0% 2% 0%',
             position: "relative",
@@ -100,22 +101,119 @@ const useStyles = makeStyles((theme: Theme) =>
         inactiveItemLink: {
             textDecoration: "none",
             color: "inherit",
+        },
+        form: {
+            padding: "0% 0px 5% 0px",
+            width: "70%",
+            marginLeft: "49%;"
+        },
+        formControl: {
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(1),
+            minWidth: 120,
+            backgroundColor: "#FFFFFF"
+        },
+        firstTextField: {
+            marginBottom: "0%",
+            width: "100%",
+            borderBottomRightRadius: "25px",
+            borderTopRightRadius: "25px",
+            border: "1px solid",
+            backgroundColor: "#FFFFFF",
+            color: "#9B9B9B"
+        },
+        firstFormControl: {
+            borderBottomRightRadius: "25px",
+            borderTopRightRadius: "25px",
+            width: "47%",
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(1),
+        },
+        root: {
+            "& .MuiFormLabel-root": {
+                color: "#EEE"
+            },
+            "&.MuiFormLabel-root.Mui-focused": {
+                  color: "#EEE"
+            },
+            "& .MuiInputBase-root": {
+                color: "#EEE"
+            },
+            "& .MuiOutlinedInput-root": {
+                border: "1px solid #EEE",
+                color: "#EEEEEE !important"
+            },
+            "& .MuiIconButton-root": {
+                color: "#EEEEEE"
+            },
+            color: "#EEE"
+        },
+        searchBtn: {
+            padding: "0px"
         }     
     }),
 );
 
+    
+
 export const AdvertisementSlider2: React.FC = function AdvertisementSlider2() {
     const classes = useStyles();
+    var { value }  = useAppData();
     const [values, setValues] = React.useState<State>({
-        email: '',
-        password: '',
-        showPassword: false,
+        address: 'Select Location',
+        searchquery: '',
       });
     
       var history = useHistory();
 
       const matches = useMediaQuery("(max-width:8000px)");
-    
+      
+      const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+
+      const handleChange2 = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setValues({...values, address: event.target.value as string});
+      };
+
+    var handleSubmit = async function handleSubmit(event) {
+        event.preventDefault();
+        //prevents default form refresh
+        //console.log("I am inside fuction");
+        try{
+            // setSuccess('');
+            // setError('');
+            // setLoading(true);
+            // await signup(values, value).then(async function(res1){
+            //     if(res1 != null){
+            //         if(res1 !== "The email address is already in use by another account."){
+            //             await fetchUserDetailsSignUp(res1).then(function(res){
+            //                 if(res){
+            //                     //console.log("About to navigate to dashboard.");
+            //                     //console.log(userRolef);
+            //                     setSuccess('Sign Up Successful.');
+            //                     setTimeout(() => {
+            //                         setSuccess('');
+            //                         console.log("about to go to dashboard");
+            //                         history.push('/Dashboard')
+            //                     }, 1500);
+            //                 }else{
+            //                     setError('Unable to login at this time'); 
+            //                 } 
+            //             });
+            //         }else{
+            //             setError('The email address is already in use by another account.')
+            //         }
+            //     }else{
+            //         setError('Unable to Sign Up at this time.'); 
+            //     }
+            // });
+            
+        }catch{
+            // setError('Failed to Sign Up');
+        }
+        //setLoading(false);
+    }
       
     return (
         <>
@@ -134,6 +232,72 @@ export const AdvertisementSlider2: React.FC = function AdvertisementSlider2() {
                                 <Typography className={`${classes.fonts} ${classes.sliderSlogan}`}>
                                     Delicious Healthy Food
                                 </Typography>
+                                <form onSubmit={handleSubmit} className={classes.form} noValidate autoComplete="off">
+                                    <FormControl variant="filled" className={classes.formControl}>
+                                        {/* <InputLabel id="demo-simple-select-filled-label">Select Location</InputLabel> */}
+                                        <Select
+                                            labelId="demo-simple-select-filled-label"
+                                            id="demo-simple-select-filled"
+                                            value={values.address}
+                                            onChange={handleChange2}
+                                            startAdornment={
+                                                <InputAdornment position="start">
+                                                    <IconButton style={{color: "#FF5E14"}}>
+                                                        <LocationOnRounded />
+                                                    </IconButton>
+                                                </InputAdornment>}
+                                        >
+                                            <MenuItem value="Select Location">
+                                                <em>Select Location</em>
+                                            </MenuItem>
+                                            <MenuItem value={10}>Ten</MenuItem>
+                                            <MenuItem value={20}>Twenty</MenuItem>
+                                            <MenuItem value={30}>Thirty</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    
+                                    <FormControl variant="outlined" className={classes.firstFormControl}>
+                                        {/* <OutlinedInput 
+                                            className={clsx(classes.firstTextField, classes.root)}
+                                            id="fullname"
+                                            type="text"
+                                            //value={values.fullname}
+                                            //onChange={handleChange('fullname')}
+                                            startAdornment={
+                                                <InputAdornment position="start">
+                                                    <IconButton color="secondary">
+                                                         <PersonRounded />
+                                                    </IconButton>
+                                                </InputAdornment>}
+                                            labelWidth={103}
+                                            required={true}
+                                        /> */}
+                                        <TextField 
+                                            id="filled-basic"  
+                                            variant="filled"
+                                            value={values.searchquery} 
+                                            onChange={handleChange('searchquery')}
+                                            className={clsx(classes.firstTextField, classes.root)}
+                                            placeholder="Search restaurants or dishes."
+                                        />
+                                    </FormControl>
+                                    <IconButton
+                                        color="inherit"
+                                        aria-label="open drawer"
+                                        edge="start"
+                                        className={classes.searchBtn}
+                                        type="submit"
+                                    >
+                                        <img src="Images/searchicon.png" style={{width: "100%"}} alt="icon"/>
+                                    </IconButton>
+                                    {/* <Button variant="contained" 
+                                        style={{backgroundColor: "#FEC109"}}
+                                         className={classes.loginButton} 
+                                         type="submit"
+                                    >
+                                        Sign Up
+                                    </Button> */}
+                                </form>
                                 <Typography className={`${classes.fonts} ${classes.sliderText}`}>
                                 With Urged, Quality and Time is our main priority. You can trust us to be on-time with your food from any restaraunt.
                                 </Typography>
