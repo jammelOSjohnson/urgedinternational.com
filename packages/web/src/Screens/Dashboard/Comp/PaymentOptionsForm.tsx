2fcb7bc7f1065e6524694ba43d7fb42463bbf937
Dwayne Redwood <buttonsmashstudios@gmail.com>
2021-11-05 19:23:46 -0500

    added images to image folder. Added form items

--- packages/web/src/Screens/Dashboard/Comp/PaymentOptionsForm.tsx
+++ packages/web/src/Screens/Dashboard/Comp/PaymentOptionsForm.tsx
@@ -1,9 +1,28 @@
 import { useAppData } from '../../../Context/AppDataContext';
-import { makeStyles, createStyles, Typography, Theme, Card, CardMedia, CardContent } from '@material-ui/core';
+import { makeStyles, createStyles, Typography, Theme, Card, CardMedia, CardContent, Grid, Paper, Divider, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField, Button } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles((theme: Theme) => 
     createStyles({
+        root: {
+
+        },
+        formHeading: {
+            fontSize: "25px",
+            fontWeight: 400,
+            lineHeight: "21px",
+            color: "#1D2635",
+            fontFamily: "Roboto",
+        },
+        paper: {
+            borderRadius: "33px"
+        },
+        divider: {
+            height: "3px"
+        },
+        form: {
+            marginLeft: "5%"
+        },
         cardTitle: {
             fontSize: "20px",
             fontWeight: 700,
@@ -42,7 +61,13 @@ const useStyles = makeStyles((theme: Theme) =>
             color: "#13ADD1",
             fontFamily: "PT Sans",
             fontWeight: "bold"
-        }
+        },
+        button: {
+            margin: theme.spacing(1),
+            color: "#FFFFFF",
+            backgroundColor: "#FF5E14",
+            borderRadius: "22px"
+        },
     }),
 );
 
@@ -59,7 +84,7 @@ export const PaymentOptionsForm: React.FC = function PaymentOptionsForm() {
       
     return (
         <>
-             <Card className={classes.card}>
+             {/* <Card className={classes.card}>
                 <CardMedia className={classes.cardImage}>
                     <img src="Images/MediumSpaceShip.png" alt="MediumSpaceShip"></img>
                     <Typography variant="h2" className={classes.OrderResult1}>{TotalOrders}</Typography>
@@ -69,7 +94,67 @@ export const PaymentOptionsForm: React.FC = function PaymentOptionsForm() {
                         Total Orders
                     </Typography>
                 </CardContent>
-            </Card>
+            </Card> */}
+            <div>
+                <Paper elevation={3} className={classes.paper}>
+                    <Grid container direction="row" spacing={2} alignItems="center"> 
+                        <Grid item xs={12}>         
+                            <form className={classes.form}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={12}>
+                                    <Typography>Payment Options</Typography>
+                                    <Divider variant="middle" className={classes.divider}/>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControl component="fieldset">
+                                        <FormLabel component="legend">Please select payment method from the list below.</FormLabel>
+                                        <RadioGroup row aria-label="position" name="position" defaultValue="top">
+                                            <FormControlLabel
+                                            value="Visa"
+                                            control={<Radio color="primary" />}
+                                            label="Start"
+                                            labelPlacement="start"
+                                            />
+                                            
+                                            <FormControlLabel
+                                            value="MasterCard"
+                                            control={<Radio color="primary" />}
+                                            label="Start"
+                                            labelPlacement="start"
+                                            />
+                                            
+                                            <FormControlLabel
+                                            value="Cash on Delivery"
+                                            control={<Radio color="primary" />}
+                                            label="Start"
+                                            labelPlacement="start"
+                                            />
+                                        </RadioGroup>
+                                    </FormControl>
+                                </Grid>
+                                <Grid container item xs={12}>
+                                <FormLabel component="legend">Delivery Address</FormLabel>
+                                    <Divider variant="middle" className={classes.divider}/>
+                                    <Grid item xs={12}>
+                                        <TextField id="street" label="Street" variant="outlined" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField id="town" label="Town" variant="outlined" />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField id="parish" label="Parish" variant="outlined" />
+                                    </Grid>
+                                    
+                                </Grid>
+                            </Grid>
+                        </form>
+                        </Grid>                        
+                            <Button variant="contained" size="large" className={classes.button}>
+                                Checkout
+                            </Button>
+                    </Grid>
+                </Paper>
+            </div>      
         </>
     )
 }
