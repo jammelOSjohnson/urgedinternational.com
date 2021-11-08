063ac5b3d3cf9ff93a697532e6739e08e5ac13d7
Dwayne Redwood <buttonsmashstudios@gmail.com>
2021-11-07 20:06:44 -0500

    card components, tyling & added buttons

--- packages/web/src/Screens/Dashboard/Comp/ShoppingCartItems.tsx
+++ packages/web/src/Screens/Dashboard/Comp/ShoppingCartItems.tsx
@@ -1,19 +1,42 @@
-import { Grid, makeStyles, createStyles, withStyles, Typography, Theme, CardMedia, Card, CardContent, Stepper, Step, StepLabel, StepIconProps, StepConnector } from '@material-ui/core';
+import { Grid, makeStyles, createStyles, withStyles, Typography, Theme, CardMedia, Card, CardContent, Stepper, Step, StepLabel, StepIconProps, StepConnector, Divider, CardHeader, IconButton } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import { PaymentOptionsForm } from './PaymentOptionsForm';
 import React from 'react';
 import clsx from 'clsx';
-
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
+            display: 'flex',
             width: '100%'
           },
+        details: {
+            display: 'flex',
+            flexDirection: 'row'
+        },
+        removeButton: {
+          backgroundColor: "#F6F6F6",
+          width: '5px',
+          height: '5px'
+
+        },
+        addButton: {
+          backgroundColor: "#FF5E14",
+          width: '5px',
+          height: '5px'
+        },
+        divider: {
+            height: "30px",
+            width: '20px',
+            backgroundColor: "#FF5E14"
+        },
         packageRoot: {
             padding: "0% 0px 2% 0px"
         },
@@ -21,7 +44,10 @@ const useStyles = makeStyles((theme: Theme) =>
             fontWeight: "bold"
         },
         image: {
-            borderRadius: '30px'
+            maxWidth: '102px',
+            maxHeight: '81px',
+            marginTop: "20px",
+            marginLeft: "12px"
         },
         card: {
           display: "flex",
@@ -33,10 +59,24 @@ const useStyles = makeStyles((theme: Theme) =>
           paddingLeft: "20px",
           paddingTop: "10px"
       },
-        cardHeading: {
+      cardHeading: {
             fontWeight: "bold",
             fontFamily: "PT Sans"
-        }
+      },
+      cardBody1: {
+        marginTop: '10px',
+        fontFamily: 'Open Sans',
+        fontWeight: 400,
+        fontSize: 14,
+        lineHeight: "12px"
+      },
+      cardBody2: {
+        marginTop: '10px',
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+        fontSize: 14,
+        lineHeight: "22px"
+      }
     }),
 );
 
@@ -146,26 +186,43 @@ function ColorlibStepIcon(props: StepIconProps) {
             <Grid container direction="row" spacing={1} className={classes.packageRoot} alignItems="flex-start">
                 <Grid item xs={7}>
                     <Card className={classes.card}>
+                      <div>
+                        <div className={classes.details}>
+                          <Typography>1. KFC Food Order</Typography>
+                          <IconButton className={classes.removeButton}>
+                            <RemoveIcon />
+                          </IconButton>
+                          <Typography>1</Typography>
+                          <IconButton className={classes.addButton}>
+                            <AddIcon />
+                          </IconButton>
+                          <IconButton className={classes.addButton}>
+                            <CloseIcon />
+                          </IconButton>
+                          <Divider variant="middle" className={classes.divider}/>
+                        </div>
+                        <Grid container direction='row' spacing={2} alignItems='flex-start'>
+                        <Grid item xs={2}>
                         <CardMedia >
                             {/* eslint-disable-next-line */}
-                            <img src="Images/Package Image.png" className={classes.image} alt="Package Image"></img>
+                            <img src="Images/menu-Big Deal.png" className={classes.image} alt="Big Deal"></img>
                         </CardMedia>
-                        <CardContent style={{flexGrow: 1}}>
+                        </Grid>
+                        <Grid item xs={10}>
+                        <CardContent>
                             <Typography className={classes.cardHeading}>
-                                Document Delivery
+                                Big Deal
                             </Typography>
-                            <Typography>
-                                Two new files were added for Service Page.
+                            <Typography className={classes.cardBody1}>
+                              3 pcs. Chicken, 1 Reg. Fries 1, Pepsi 475mL
                             </Typography>
-                          
-                                <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-                                    {steps.map((label) => (
-                                        <Step key={label}>
-                                            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-                                        </Step>
-                                    ))}
-                                </Stepper>
+                            <Typography className={classes.cardBody2}>
+                              Flavour Chicken: Original & Barbique Mixed. Flavour Drink: Grape/ Ginger Beer. Other: No Breast or Rib.
+                            </Typography>
                         </CardContent>
+                        </Grid>
+                        </Grid>
+                      </div>
                     </Card>
                 </Grid>
                 <Grid item xs={5}>
