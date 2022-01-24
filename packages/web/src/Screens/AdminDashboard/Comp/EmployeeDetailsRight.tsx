f7dcc4c31e6a83ad65c545aa077ec2650394b57c
Dwayne Redwood <dwayneredwood@live.com>
2022-01-24 01:30:23 -0500

    Added Card for Employee

--- packages/web/src/Screens/AdminDashboard/Comp/EmployeeDetailsRight.tsx
+++ packages/web/src/Screens/AdminDashboard/Comp/EmployeeDetailsRight.tsx
@@ -1,7 +1,9 @@
-import { Container, Grid , makeStyles, createStyles, Theme } from '@material-ui/core';
+import { useAppData } from '../../../Context/AppDataContext';
+import { Container, Grid , makeStyles, createStyles, Theme, Card, Typography, CardHeader, Avatar, IconButton, CardMedia, CardContent } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 // import Visibility from '@material-ui/icons/Visibility';
 // import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import React from 'react';
+import React, { useEffect } from 'react';
 // import { useHistory } from 'react-router-dom';
 // import clsx from 'clsx';
 // import { NotificationImportantRounded, ShoppingCartRounded } from "@material-ui/icons/";
@@ -27,11 +29,74 @@ const useStyles = makeStyles((theme: Theme) =>
         gridRoot: {
             padding: "0px"
         },
+        cardTitle: {
+            fontSize: "20px",
+            fontWeight: 700,
+            color: "#1D2635",
+            fontFamily: "PT Sans",
+        },
+        cardContent: {
+            flexGrow: 1,
+            textAlign: "left",
+            paddingBottom: "0px",
+            paddingTop: "0px",
+        },
+        cardImage: {
+            textAlign: "left",
+            position: "relative"
+        },
+        card: {
+            background: "#FFFFFF",
+            border: "0.813791px solid #E2E2E2",
+            boxSizing: "border-box",
+            boxShadow: "0px 4.64215px 12.2069px rgba(0, 0, 0, 0.11)",
+            borderRadius: "34.3745px",
+        },
+        avatar: {
+            width: "105px",
+            height: "105px",
+            backgroundColor: "#FFFFFF",
+            marginLeft: "auto", 
+            marginRight: "auto"
+          },
+        kfcImage: {
+            width: "105px",
+            height: "105px",
+            margin: "0% 0% 14% 8%",
+        },
+        statusDot: {
+            height: "10px",
+            width: "10px",
+            backgroundColor: "#22F810" /*Active*/ ,
+            /*backgroundColor: "#F86363", In-Active*/
+            borderRadius: "50%",
+            display: "inline-block"
+        },
+        statusDotI: {
+            height: "10px",
+            width: "10px",
+            backgroundColor: "red" /*Active*/ ,
+            /*backgroundColor: "#F86363", In-Active*/
+            borderRadius: "50%",
+            display: "inline-block"
+        }
     }),
 );
 
 export const EmployeeDetailsRight: React.FC = function EmployeeDetailsRight() {
     const classes = useStyles();
+
+    var { value }  = useAppData();
+    var { fetchRiders, riders, viewMenuItems } = value;
+    
+    useEffect(() => {
+        //console.log("inside use effect");
+        //console.log(restaurants);
+        if(riders.length === 0){
+            fetchRiders(value);
+        }
+    }, [riders])
+
     // const [values, setValues] = React.useState<State>({
     //     email: '',
     //     password: '',
@@ -49,14 +114,63 @@ export const EmployeeDetailsRight: React.FC = function EmployeeDetailsRight() {
                     <Grid item xs={12} spacing={1}>
                         Calendar
                     </Grid>
-                    <Grid item xs={12} spacing={1}style={{marginTop: "100%"}}>
-                        Employee Card
+                    <Grid item xs={12} spacing={1}>
+                            <Card className={classes.gridRoot}>
+                                <CardHeader
+                                    action={
+                                        <IconButton aria-label="settings">
+                                            <MoreVertIcon />
+                                        </IconButton>
+                                    }
+                                />
+                                <CardContent>
+                                    <Grid container xs={12} direction="row" spacing={1} className={classes.gridRoot} alignItems="center">
+                                        <Grid item xs={12}>
+                                            <Avatar variant="circle" aria-label="rider" className={classes.avatar}>
+                                                <CardMedia className={classes.cardImage}>
+                                                    {/* <img className={classes.kfcImage} src={rider.ImageName}></img> */}
+                                                    <img className={classes.kfcImage} src="Images/KFC Avatar.png"></img> 
+                                                </CardMedia>
+                                            </Avatar>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Typography variant="h5"  component="p" align="center">
+                                                {/* {rider.FirstName} */}
+                                                Dilan
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Typography variant="h6"  component="p" align="center">
+                                                {/* {rider.City} */}
+                                                Kingston
+                                            </Typography>
+                                        </Grid>
+                                        <br />
+                                        <Grid item xs={12}>
+                                            <Typography variant="body2"  component="p" align="center">
+                                                {/* {rider.Email} */}
+                                                Dilan.Po@gmail.com
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Typography variant="body2"  component="p" align="center">
+                                            Errand Runner
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Typography variant="body2"  component="p" align="center">
+                                            {/* {rider.isAvailable?
+                                                <span><span className={classes.statusDot}></span> Active</span>
+                                                :
+                                                <span><span className={classes.statusDotI}></span> In-active</span>
+                                            } */}
+                                            <span><span className={classes.statusDot}></span> Active</span>
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </CardContent>
+                            </Card>
                     </Grid>
-                    {/* <Grid container direction="row" xs={6} spacing={1}>
-                        <Grid item xs={12} spacing={0}>
-                            Employee Card
-                        </Grid>
-                    </Grid> */}
                 </Grid>
             </Container>
         </>
