1a4bc5b28ae17460cd3bb46e3d18e0e7e4242636
jammelOSjohnson <jammeljohnson@gmail.com>
2023-07-05 21:04:19 -0500

    fix spinner

--- packages/web/src/Components/spinner.tsx
+++ packages/web/src/Components/spinner.tsx
@@ -1,37 +1,42 @@
-import { Backdrop, CircularProgress, createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
+import {
+  Backdrop,
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 
-
-const useStyles = makeStyles((theme: Theme) => 
-    createStyles({
-        spinner: {
-            paddingTop: 0,
-            paddingBottom: 0,
-            height: "80vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-        },
-        backdrop: {
-            zIndex: theme.zIndex.drawer + 1,
-            color: '#fff',
-        },
-    }),
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    spinner: {
+      paddingTop: 0,
+      paddingBottom: 0,
+      height: "80vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: "#fff",
+    },
+  })
 );
 export const Spinner: React.FC = function Spinner() {
-    const classes = useStyles();
-    //@typescript-eslint/no-unused-vars
-    const [open, setOpen] = React.useState(true);
+  const classes = useStyles();
+  //@typescript-eslint/no-unused-vars
+  const [open, setOpen] = React.useState(true);
 
-    const handleClose = () => {
-        //setOpen3(false);
-    };
-    return (
-        <Typography variant="body1" className={classes.spinner}>
-            <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        </Typography>
-    )
-
-}
+  const handleClose = () => {
+    //setOpen3(false);
+  };
+  return (
+    <div className={classes.spinner}>
+      <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+        <CircularProgress color="secondary" />
+      </Backdrop>
+    </div>
+  );
+};
