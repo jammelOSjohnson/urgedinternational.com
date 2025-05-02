4202521ab68aafdacc9b15d6f85317b0d06dfc4a
jammelOSjohnson <jammeljohnson@gmail.com>
2025-05-02 10:55:23 -0500

    Refactor Section4 component: - Improved code formatting and readability - Added Hidden component for responsive design on smaller screens

--- packages/web/src/Screens/Home/Comp/Section4.tsx
+++ packages/web/src/Screens/Home/Comp/Section4.tsx
@@ -1,81 +1,92 @@
-import React from 'react';
+import React from "react";
 //import CSS
-import { Container, makeStyles, createStyles, Theme, Typography, Grid, useMediaQuery, useTheme} from '@material-ui/core';
+import {
+  Container,
+  makeStyles,
+  createStyles,
+  Theme,
+  Typography,
+  Grid,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import Hidden from "@material-ui/core/Hidden";
 
-const useStyles = makeStyles((theme: Theme) => 
-    createStyles({
-        cardGrid3: {
-            padding: "5% 0 10% 0",
-            color: "#1D2635",
-        },
-        excitedGirl: {
-            zIndex: -1,
-            width: "320px",
-            position: "relative",
-            bottom: "-115px",
-        },
-        text1: {
-            fontWeight: 600,
-            fontSize: "2.5rem",
-            fontFamily: "Inter",
-            textAlign: "center",
-            paddingBottom: "2%"
-        },
-        text2: {
-            fontWeight: 300,
-            fontSize: "1.5rem",
-            fontFamily: "Inter",
-            textAlign: "center",
-            paddingBottom: "2%"
-        }
-    }),
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    cardGrid3: {
+      padding: "5% 0 10% 0",
+      color: "#1D2635",
+    },
+    excitedGirl: {
+      zIndex: -1,
+      width: "320px",
+      position: "relative",
+      bottom: "-115px",
+    },
+    text1: {
+      fontWeight: 600,
+      fontSize: "2.5rem",
+      fontFamily: "Inter",
+      textAlign: "center",
+      paddingBottom: "2%",
+    },
+    text2: {
+      fontWeight: 300,
+      fontSize: "1.5rem",
+      fontFamily: "Inter",
+      textAlign: "center",
+      paddingBottom: "2%",
+    },
+  })
 );
 
 export const Section4: React.FC = function Section4() {
-    const classes = useStyles();
-    const theme = useTheme();
-    
-    const isMatchMedium = useMediaQuery(theme.breakpoints.up('md'));
-    const isMatch = useMediaQuery(theme.breakpoints.up('sm'));
+  const classes = useStyles();
+  const theme = useTheme();
 
-    return (
-        <>
-            <Container maxWidth="xl" className={classes.cardGrid3}>
-                <Typography variant="caption">
-                    <Typography className={classes.text1}>And many more...</Typography>
-                </Typography>
-                <Typography variant="caption">
-                    <Typography className={classes.text2}>
-                        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  const isMatchMedium = useMediaQuery(theme.breakpoints.up("md"));
+  const isMatch = useMediaQuery(theme.breakpoints.up("sm"));
+
+  return (
+    <>
+      <Hidden smDown>
+        <Container maxWidth="xl" className={classes.cardGrid3}>
+          <Typography variant="caption">
+            <Typography className={classes.text1}>And many more...</Typography>
+          </Typography>
+          <Typography variant="caption">
+            <Typography className={classes.text2}>
+              {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Vitae cursus et<br /> euismod tempor. Adipiscing elementum vestibulum in eget
                          enim donec<br /> sed tincidunt. */}
-                    </Typography>
+            </Typography>
+          </Typography>
+          <Grid container spacing={0}>
+            {isMatchMedium ? (
+              <Grid item xs={12} md={6}>
+                <Typography style={{ textAlign: "center" }}>
+                  <img src="Images/Samsung Galaxy A50.webp" alt="urged phone" />
                 </Typography>
-                <Grid container spacing={0}>
-                    {isMatchMedium?
-                        <Grid item xs={12} md={6}>
-                            <Typography style={{textAlign: "center"}}>
-                                <img src="Images/Samsung Galaxy A50.webp" alt="urged phone" />
-                            </Typography>
-                        </Grid>
-                    :
-                        <></>
-                    }
-                    
-                    {isMatch?
-                        <Grid item xs={12} md={6}>
-                            <Typography style={{textAlign: "center"}}>
-                                <img src="Images/Logos.webp" alt="restaurants" />
-                            </Typography>
-                        </Grid>
-                    :
-                        <Grid item xs={12} md={6}>
-                            <img src="Images/Logos.webp" width="100%" alt="restaurants" />
-                        </Grid>
-                    }
-                    
-                </Grid>
-            </Container>
-        </>
-    )
-}
+              </Grid>
+            ) : (
+              <></>
+            )}
+
+            {isMatch ? (
+              <Grid item xs={12} md={6}>
+                <Typography style={{ textAlign: "center" }}>
+                  <img src="Images/Logos.webp" alt="restaurants" />
+                </Typography>
+              </Grid>
+            ) : (
+              <Grid item xs={12} md={6}>
+                <img src="Images/Logos.webp" width="100%" alt="restaurants" />
+              </Grid>
+            )}
+          </Grid>
+        </Container>
+      </Hidden>
+    </>
+  );
+};
