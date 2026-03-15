54f52c5ef3086c9715316b1eef66cae6da0dd5f9
jammelOSjohnson <jammeljohnson@gmail.com>
2026-03-15 13:32:58 -0500

    Add .npmrc and .prettierignore files, update Firebase functions to v2, and upgrade package dependencies to Node 22

--- packages/web/functions/index.js
+++ packages/web/functions/index.js
@@ -1,18 +1,13 @@
-const functions = require("firebase-functions");
+const {onCall} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-exports.creatRestauranteUser = functions.https.onCall((data) => {
-  return admin.auth().createUser(data)
+// 2nd gen callable (Node 22)
+// clients still call httpsCallable("creatRestauranteUser")
+exports.creatRestauranteUser = onCall((request) => {
+  return admin
+      .auth()
+      .createUser(request.data)
       .catch((error) => {
         const {code, details} = JSON.parse(JSON.stringify(error));
         console.log("CREATE_RESTAURANT_ERROR", code, details);
