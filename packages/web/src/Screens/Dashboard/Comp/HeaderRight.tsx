ad4f1ac1b47d4fe783256a5cf900f3f8ce303f26
jammelOSjohnson <jammeljohnson@gmail.com>
2026-03-15 13:45:59 -0500

    Refactor HeaderRight component for improved readability, including consistent formatting and style adjustments, while maintaining functionality for scroll behavior and conditional rendering.

--- packages/web/src/Screens/Dashboard/Comp/HeaderRight.tsx
+++ packages/web/src/Screens/Dashboard/Comp/HeaderRight.tsx
@@ -12,63 +12,74 @@ const sx = {
 };
 
 export const HeaderRight: React.FC = function HeaderRight() {
-    const [scrolled, setScrolled] = React.useState(false);
-    var history = useHistory();
-    var location = history.location;
-    var referralPath = location.pathname;
+  const [scrolled, setScrolled] = React.useState(false);
+  var history = useHistory();
+  var location = history.location;
+  var referralPath = location.pathname;
 
-    const handleScroll = () => {
-        let container = document.getElementById("right-container");
-        let cart = document.getElementById("cart-icon-header-right");
-        let scrollY = window.scrollY;
-        //console.log('scrolled', scrollY)
-        if (scrollY > 0) {
-            setScrolled(true);
-            if(!(container?.classList.contains("scrolled"))){
-                container?.classList.toggle("scrolled");
-            }
-            if(!(cart?.classList.contains("cart-scrolled"))){
-                cart?.classList.toggle("cart-scrolled");
-            }
-        } else {
-            setScrolled(false);
-            if(container?.classList.contains("scrolled")){
-                container.classList.toggle("scrolled");
-            }
+  const handleScroll = () => {
+    let container = document.getElementById("right-container");
+    let cart = document.getElementById("cart-icon-header-right");
+    let scrollY = window.scrollY;
+    //console.log('scrolled', scrollY)
+    if (scrollY > 0) {
+      setScrolled(true);
+      if (!container?.classList.contains("scrolled")) {
+        container?.classList.toggle("scrolled");
+      }
+      if (!cart?.classList.contains("cart-scrolled")) {
+        cart?.classList.toggle("cart-scrolled");
+      }
+    } else {
+      setScrolled(false);
+      if (container?.classList.contains("scrolled")) {
+        container.classList.toggle("scrolled");
+      }
 
-            if(cart?.classList.contains("cart-scrolled")){
-                cart?.classList.toggle("cart-scrolled");
-            }
-        }
+      if (cart?.classList.contains("cart-scrolled")) {
+        cart?.classList.toggle("cart-scrolled");
+      }
     }
+  };
 
-    useEffect(() => {
-        handleScroll();
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    })
-    if(!scrolled){
-        return (
-            <>
-                <Container id="right-container" maxWidth="xl" sx={{ ...sx.main, ...sx.notScrolled }} onScroll={handleScroll}>
-                    <Grid container direction="row" spacing={0} sx={sx.gridRoot} alignItems="center">
-                        <Grid item xs={6}>
-                            <User />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Grid container direction="row" spacing={0}>
-                                <Grid item xs={12} style={{marginTop: "10%"}}>
-                                    <Notification /> <span style={{marginRight: "10%"}}></span>
-                                    <Cart />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Container>
-                <style>
-                    {`
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+  if (!scrolled) {
+    return (
+      <>
+        <Container
+          id="right-container"
+          maxWidth="xl"
+          sx={{ ...sx.main, ...sx.notScrolled }}
+          onScroll={handleScroll}
+        >
+          <Grid
+            container
+            direction="row"
+            spacing={0}
+            sx={sx.gridRoot}
+            alignItems="center"
+          >
+            <Grid item xs={6}>
+              <User />
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container direction="row" spacing={0}>
+                <Grid item xs={12} style={{ marginTop: "0%" }}>
+                  <Notification /> <span style={{ marginRight: "10%" }}></span>
+                  <Cart />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+        <style>
+          {`
                         .scrolled {
                             background-color: #FFF;
                             position: fixed;
@@ -80,29 +91,40 @@ export const HeaderRight: React.FC = function HeaderRight() {
                         }
 
                     `}
-                </style>
-            </>
-        )
-    }else if(referralPath.toLowerCase() === "/shoppingcart") {
-        return (
-            <>
-                <Container id="right-container" maxWidth="xl" sx={{ ...sx.main, ...sx.notScrolled }} onScroll={handleScroll}>
-                    <Grid container direction="row" spacing={0} sx={sx.gridRoot} alignItems="center">
-                        <Grid item xs={6}>
-                            <User />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Grid container direction="row" spacing={1}>
-                                <Grid item xs={12} style={{marginTop: "10%"}}>
-                                    <Notification /> <span style={{marginRight: "10%"}}></span>
-                                    <Cart />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Container>
-                <style>
-                    {`
+        </style>
+      </>
+    );
+  } else if (referralPath.toLowerCase() === "/shoppingcart") {
+    return (
+      <>
+        <Container
+          id="right-container"
+          maxWidth="xl"
+          sx={{ ...sx.main, ...sx.notScrolled }}
+          onScroll={handleScroll}
+        >
+          <Grid
+            container
+            direction="row"
+            spacing={0}
+            sx={sx.gridRoot}
+            alignItems="center"
+          >
+            <Grid item xs={6}>
+              <User />
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container direction="row" spacing={1}>
+                <Grid item xs={12} style={{ marginTop: "10%" }}>
+                  <Notification /> <span style={{ marginRight: "10%" }}></span>
+                  <Cart />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+        <style>
+          {`
                         .scrolled {
                             // background-color: #FFF;
                             // position: fixed;
@@ -114,17 +136,17 @@ export const HeaderRight: React.FC = function HeaderRight() {
                         }
 
                     `}
-                </style>
-            </>
-        )
-    }else{
-        return (
-            <>
-                <div id='right-container'>
-                    <Cart />
-                </div>
-                <style>
-                    {`
+        </style>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div id="right-container">
+          <Cart />
+        </div>
+        <style>
+          {`
                         .scrolled {
                             position: fixed;
                             z-index: 2;
@@ -143,8 +165,8 @@ export const HeaderRight: React.FC = function HeaderRight() {
                         }
 
                     `}
-                </style>
-            </>
-        )
-    }
-}
+        </style>
+      </>
+    );
+  }
+};
